f4aee741294993539c89d38000b59169f622a16d
Mohdan <mohdan.barzankaev@gmail.com>
2021-06-29 21:34:12 +0300

    page help

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,12 +1,28 @@
 <template>
-  <app-page title="О странице">
-
+  <app-page :back="back = true" title="Ответы а вопросы">
+    <question-item
+      v-for='q in questions'
+      :key="q.title"
+      :question="q"
+    ></question-item>
   </app-page>
 </template>
 
 <script>
 import AppPage from '../components/ui/AppPage'
+import QuestionItem from '../components/questions/QuestionItem'
+
 export default {
-  components: { AppPage }
+  setup() {
+    const questions = [
+      {title: 'Что есть в данной ситстеме?', text: 'Тут есть все, для того, чтобы смотерть какие транзакции человек совершал'},
+      {title: 'Как тут рабоатет авторизаця?', text: 'Все реализовано через Firebase'}
+    ]
+
+    return {
+      questions
+    }
+  },
+  components: { AppPage, QuestionItem }
 }
 </script>
